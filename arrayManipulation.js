8d3faa30ed335ba6d92bc493bc8c99dde6392a05
JD-2104 <jdanso019@st.ug.edu.gh>
2024-05-18 04:19:04 +0000

    TASK 1

---
+++ arrayManipulation.js
@@ -0,0 +1,12 @@
+// TASK 1
+function processArray(arr) {
+    return arr.map(num => {
+        if (num % 2 === 0) {
+            return num * num;
+        } else {
+            return num * 3;
+        }
+    });
+}
+
+module.exports = processArray;
