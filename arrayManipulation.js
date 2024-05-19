dcd3e2a0776fc340c6a0b1c4c278313ea5d6dc76
JD-2104 <jdanso019@st.ug.edu.gh>
2024-05-19 08:30:21 +0000

    TASK 2

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -9,6 +9,31 @@ function processArray(numbers) {
     });
 }
 
+
+
+
+// TASK 2
+function formatArrayStrings(stringsArray, numbersArray) {
+    if (stringsArray.length !== numbersArray.length) {
+        throw new Error("Arrays must have the same length");
+    }
+
+    for (let i = 0; i < stringsArray.length; i++) {
+        const string = stringsArray[i];
+        const number = numbersArray[i];
+
+        if (number % 2 === 0) {
+            stringsArray[i] = string.toUpperCase(); 
+        } else {
+            stringsArray[i] = string.toLowerCase(); 
+        }
+    }
+
+    return stringsArray;
+}
+
 module.exports = {
-    processArray
+    processArray,
+    formatArrayStrings
 };
+
