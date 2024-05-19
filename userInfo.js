9aab90e9cb1310b4ed01ad8001511ccd70955d06
JD-2104 <jdanso019@st.ug.edu.gh>
2024-05-19 08:33:18 +0000

    TASK 3

---
+++ userInfo.js
@@ -0,0 +1,15 @@
+// TASK 3
+function createUserProfiles(names, modifiedNames) {
+    let id = 1;
+    return names.map((name, index) => {
+        return {
+            originalName: name,
+            modifiedName: modifiedNames[index],
+            id: id++
+        };
+    });
+}
+
+module.exports = {
+    createUserProfiles
+};
